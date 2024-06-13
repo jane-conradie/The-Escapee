9872fab4400ae19e17783a2e18f6c7f0061cab27
jane_conradie <jconradie1998@gmail.com>
2024-06-13 12:15:02 +0200

    implement running and flipping sprite based on direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 8f;
+
     Vector2 moveInput;
+
+    Rigidbody2D rigidbody2D;
+    Animator animator;
 
     void Start()
     {
-
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        Run();
+        FlipSprite();
+    }
 
+    void FlipSprite()
+    {
+        // mathf epsilon means 0
+        bool playerIsMoving = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        if (playerIsMoving)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), 1f);
+        }
+
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = playerVelocity;
+
+        bool playerIsMoving = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", playerIsMoving);
     }
 
     void OnMove(InputValue value)
